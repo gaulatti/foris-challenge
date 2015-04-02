a702199278a6c084344684d44edcbda2a991f06c
Javier Godoy <rotvulpix@gmail.com>
2015-04-02 20:18:25 -0300

    Palíndromo en Ruby

---
+++ ruby/palindromo.rb
@@ -0,0 +1,19 @@
+def palindromo()
+	texto = File.new("../string.txt", "r").gets
+	palindromo = ""
+
+	for i in 0..texto.length
+		for j in 0..texto.length
+			if (j + i) <= texto.length
+				fragmento = texto[j..i]
+				if(fragmento == fragmento.reverse) & (fragmento.length > palindromo.length)
+					palindromo = fragmento
+				end
+			end
+		end
+	end
+	puts palindromo
+
+end
+
+palindromo()
