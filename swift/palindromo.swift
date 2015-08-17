9b45d38e9678a11887129f9abe6c2766e26871f3
Javier Godoy <rotvulpix@gmail.com>
2015-08-17 00:03:13 -0300

    Featuring Swift: Palindromo, very slow

--- swift/palindromo.swift
+++ swift/palindromo.swift
@@ -1,19 +1,25 @@
 import Foundation
+extension String {
+    subscript (r: Range<Int>) -> String {
+        get {
+            let startIndex = advance(self.startIndex, r.startIndex)
+            let endIndex = advance(startIndex, r.endIndex - r.startIndex)
+
+            return self[Range(start: startIndex, end: endIndex)]
+        }
+    }
+}
 func palindromo() {
     let file = "../string.txt".stringByExpandingTildeInPath
     let str = NSString(contentsOfFile: file, encoding: NSUTF8StringEncoding, error: nil) as! String
     let length = count(str)
     var palindromoActual: String = ""
     for i in 0...length {
-        for j in 2...length {
-            if ((j + i) <= length) {
-                var fragmento = str[advance(str.startIndex, i)..<advance(str.startIndex, i + j)]
-                if(palindromo(fragmento) && count(fragmento) >= count(palindromoActual)) {
-                    palindromoActual = fragmento
-                }
-            }
+        for character in str.subStringWithRange() {
+
         }
     }
+
     println(palindromoActual)
 }
 
