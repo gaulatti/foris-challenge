8aa342ced774b0c119d6d4a0973517e1561723eb
Javier Godoy Núñez <rotvulpix@gmail.com>
2016-10-20 17:01:34 -0300

    Update fibonacci.rb

--- ruby/fibonacci.rb
+++ ruby/fibonacci.rb
@@ -1,24 +1,21 @@
 def fibonacci()
-	a = b = 1;
-	while true
-		temp = a; a = b; b = temp + b
-		if factors(a) >= 1000
-			puts a
-			return
-		end
-	end
+  a = b = 1;
+  while true
+    a, b = b, a + b
+    return a if factors(a) >= 1000
+  end
 end
 
 def factors(num)
-	divisores = []
-	n = 1
-	while n <= num ** 0.5
-		if num.modulo(n) == 0
-			divisores << divisores << num / n unless num / n == n
-		end
-		n += 1
-	end
-	return divisores.length
+  divisores = []
+  n = 1
+  while n <= num ** 0.5
+    if num.modulo(n) == 0
+      divisores << divisores << num / n unless num / n == n
+    end
+    n += 1
+  end
+  return divisores.length
 end
 
-fibonacci()
+puts fibonacci()
