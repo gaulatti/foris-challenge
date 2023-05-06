b371201728e62d5c05d3bef1d08c78bd56106176
Javier Godoy Núñez <jack@gaulatti.com>
2023-05-06 13:27:15 -0400

    Update fibonacci.cpp

--- c++/fibonacci.cpp
+++ c++/fibonacci.cpp
@@ -1,27 +1,29 @@
 #include <iostream>
-#include <math.h>
+#include <vector>
 using namespace std;
 
-int factores(long int numero) {
-	int cuenta = 0;
-	for(int i = 1; i <= sqrt(numero); i = i+1){
-		if(numero % i == 0) {
-			cuenta = cuenta + 1;
-			if(i != numero/i) {
-				cuenta = cuenta + 1;
-			}
-		}
-	}
-	return cuenta;
+int get_factors_count(long long int number) {
+    int count = 0;
+    for (int i = 1; i * i <= number; ++i) {
+        if (number % i == 0) {
+            count += 2;
+            if (i * i == number) {
+                --count;
+            }
+        }
+    }
+    return count;
 }
 
 int main() {
-	long int a = 1; long int b = 1; long int temp = 0;
-	while(true) {
-		temp = a; a = b; b = temp + b;
-		if(factores(a) >= 1000) {
-			cout << a;
-			return 0;
-		}
-	}
+    long long int a = 1, b = 1, temp = 0;
+    while (true) {
+        temp = a; 
+        a = b; 
+        b = temp + b;
+        if (get_factors_count(a) >= 1000) {
+            cout << a << endl;
+            return 0;
+        }
+    }
 }
